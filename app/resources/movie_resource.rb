111d26698a71680e34d67aeb1ab6121103efe6a8
anonymous anonymous <caleb.baltes@kellogg.northwestern.edu>
2021-11-14 23:24:35 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,16 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :year, :string
+  attribute :description, :string
+  attribute :director_id, :integer
+  attribute :duration, :integer
+  attribute :image, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
