f196c8597e40f94edd1532abd759a109d9b09988
anonymous anonymous <caleb.baltes@kellogg.northwestern.edu>
2021-11-14 23:24:43 +0000

    Generate graphiti resources for reviews

---
+++ app/resources/review_resource.rb
@@ -0,0 +1,14 @@
+class ReviewResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :stars, :integer
+  attribute :movie_id, :integer
+  attribute :user_id, :integer
+  attribute :review_description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
